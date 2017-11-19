b9e8dbb5f457fb2423aef56aa25f2880baa40c99
Stefan Reinhard <stefan@monostream.com>
2017-11-19 19:25:58 +0100

    Cleanup & Use images converted to jpeg on iOS 11 as reference

--- test/tests.py
+++ test/tests.py
@@ -8,83 +8,83 @@ import os, sys, subprocess
 test_dir = os.path.dirname(os.path.realpath(__file__))
 
 build_dir = '../build'
-tifig_bin = build_dir + '/tifig'
+tifig_bin = os.path.join(build_dir, 'tifig')
 fixtures_dir = '../fixtures'
+references_dir = 'iOS11_ref_exports'
 
 ssim_min_value = 0.90
 
 def run(command):
-	return subprocess.call(command, shell=True)
+    return subprocess.call(command, shell=True)
 
 def build_tifig():
-	if os.path.exists(tifig_bin):
-		print('> tifig binary found', color='green')
-		return
+    if os.path.exists(tifig_bin):
+        print('> tifig binary found', color='green')
+        return
 
-	print('Building tifig', color='yellow')
+    print('Building tifig', color='yellow')
 
-	if not os.path.isdir(build_dir):
-		os.mkdir(build_dir)
+    if not os.path.isdir(build_dir):
+        os.mkdir(build_dir)
 
-	os.chdir(build_dir)
+    os.chdir(build_dir)
 
-	cmake_ret = run('cmake ..')
-	make_ret =run('make')
+    cmake_ret = run('cmake ..')
+    make_ret =run('make')
 
-	if not (cmake_ret == 0 and make_ret == 0 and os.path.exists(tifig_bin)):
-		print('Failed to build tifig', color='red')
-		exit(1)
+    if not (cmake_ret == 0 and make_ret == 0 and os.path.exists(tifig_bin)):
+        print('Failed to build tifig', color='red')
+        exit(1)
 
-	os.chdir(test_dir)
+    os.chdir(test_dir)
 
 def get_test_candidates():
-	fixtures = os.listdir(fixtures_dir)
-	basenames = sorted({os.path.splitext(f)[0] for f in fixtures})
-	for f in basenames:
-		if f + '.heic' in fixtures and f + '.jpg' in fixtures:
-			heic = '%s/%s.heic' % (fixtures_dir, f)
-			ref = '%s/%s.jpg' % (fixtures_dir, f)
-			converted = f + '_tifig.tiff'
-			yield((heic, ref, converted, f))
+    fixtures = sorted(os.listdir(fixtures_dir))
+    references = sorted(os.listdir(references_dir))
+
+    for heic, ref_jpg in zip(fixtures, references):
+        heic_path = os.path.join(fixtures_dir, heic)
+        ref_path = os.path.join(references_dir, ref_jpg)
+        yield((heic_path, ref_path, ref_jpg))
 
 
 def run_similarity_test():
-	for heic, ref, converted, name in get_test_candidates():
-		print('> Converting %s => %s' % (name + '.heic', converted), color='yellow')
-		tifig_ret = run('%s -v %s %s' % (tifig_bin, heic, converted))
-		if (tifig_ret != 0):
-			print('Converting heic image with tifig failed!', color='red')
-			exit(1)
+    for heic, ref, converted in get_test_candidates():
+        print('> Converting %s => %s' % (os.path.basename(heic), converted), color='yellow')
+        tifig_ret = run('%s -v %s %s' % (tifig_bin, heic, converted))
+        if (tifig_ret != 0):
+            print('Converting heic image with tifig failed!', color='red')
+            exit(1)
 
-		print('> Comparing visual similarity', color='cyan')
-		
-		ref_img = Image.open(ref)
-		converted_img = Image.open(converted)
+        print('> Computing visual similarity', color='cyan')
+        
+        ref_img = Image.open(ref)
+        converted_img = Image.open(converted)
 
-		ssim = compute_ssim(ref_img, converted_img)
+        ssim = compute_ssim(ref_img, converted_img)
 
-		if (ssim < ssim_min_value):
-			print('Similarity: %2.1f%%' % (ssim * 100), color='red')
-			print('This below our acceptance rate of %.2f!' % ssim_min_value, color='red')
-			exit(1)
-		else:
-			print('Similarity: %2.1f%%' % (ssim * 100), color='green')
+        if (ssim < ssim_min_value):
+            print('Similarity: %2.2f%%' % (ssim * 100), color='red')
+            print('This is below our acceptance rate of %2f%%!' % ssim_min_value, color='red')
+            exit(1)
+        else:
+            print('Similarity: %2.2f%%' % (ssim * 100), color='green')
 
-		os.remove(converted)
+        os.remove(converted)
 
-		print()
+        print()
 
 
 if __name__ == "__main__":
-	print()
-	print('#######################################', color='blue')
-	print('# Running tifig regression test suite #', color='blue')
-	print('#######################################', color='blue')
-	print()
+    print()
+    print('#######################################', color='blue')
+    print('# Running tifig regression test suite #', color='blue')
+    print('#######################################', color='blue')
+    print()
 
-	# make sure this is callable from everywhere
-	os.chdir(test_dir)
+    # make sure this is callable from everywhere
+    os.chdir(test_dir)
 
-	build_tifig()
-	print()
-	run_similarity_test()
+    build_tifig()
+    print()
+    run_similarity_test()
